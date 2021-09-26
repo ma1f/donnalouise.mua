b13ba01db2e78078266453a4e30465bd939fe25e
Mark Alford <m.alford@blackfinch.com>
2021-09-26 22:33:36 +0100

    updates to photos, modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -7,7 +7,7 @@ export default function modal() {
     //return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
     console.log(scrollTop); 
     console.log(rect.top + scrollTop); 
-    return { top: rect.top, left: rect.left }
+    return { top: parseInt(rect.top), left: parseInt(rect.left) }
   };
 
   const modal = document.getElementById('modal')
@@ -22,34 +22,30 @@ export default function modal() {
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
     modal.classList.add('active');
+    modal.classList.remove('fade-out');
+    modal.classList.add('fade-in');
     body.classList.add('no-scroll');
 
     let target = evt.target;
     let pos = offset(target);
     let img = evt.target.cloneNode();
     img.classList.add('shadow');
+    img.classList.remove('modal');
     
-    image.style.padding = '0';
-    image.style.top = pos.top+'px';
-    image.style.left = pos.left+'px';
-    image.style.width = target.width+'px';
-    image.style.height = target.height+'px';
+    let padding = (pos.top)+'px '+Math.max(vw-target.width-pos.left, 0)+'px '+Math.max(vh-target.height-pos.top, 0)+'px '+(pos.left)+'px'
+    image.style.padding = padding;
+
     image.appendChild(img);
     image.classList.add('animate');
-
-    image.style.top = '0px';
-    image.style.left = '0px';
-    image.style.width = '100%';
-    image.style.height = vh+'px';
-    image.style.padding = '56px 24px 24px';
-    console.log(image.style.height); 
 
     desc.innerHTML = target.dataset.description;
  
   };
 
   const closeModal = (evt) => {
+    modal.classList.add('fade-out');
     modal.classList.remove('active');
+    modal.classList.remove('fade-in');
     body.classList.remove('no-scroll');
     image.classList.remove('animate');
     image.firstChild.remove();
