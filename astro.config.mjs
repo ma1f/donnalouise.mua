330891dbe72a6c61b72fdf94117e920da698c143
Mark Alford <m.alford@blackfinch.com>
2022-05-16 10:20:59 +0100

    pricing updates and new photos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config';
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
+import sitemap from "@astrojs/sitemap";
 
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: [],
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://donnalouisemua.com',
+  integrations: [sitemap()]
 });
